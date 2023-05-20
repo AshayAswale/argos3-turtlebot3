0bbd19e5b4458981cd10e8c90bc0b9b4e5df63d4
AshayAswale <asaswale@wpi.edu>
2023-05-19 21:22:03 -0400

    Replaced KheperaIV occurances with Turtlebot3 (cmake dir)

--- src/cmake/ARGoSBuildOptions.cmake
+++ src/cmake/ARGoSBuildOptions.cmake
@@ -26,14 +26,14 @@ if(NOT DEFINED ARGOS_BUILD_NATIVE)
 endif(NOT DEFINED ARGOS_BUILD_NATIVE)
 
 #
-# If building for real robot, check if location of Khepera IV libs has
+# If building for real robot, check if location of Turtlebot 3 libs has
 # been given
 #
 if(NOT ARGOS_BUILD_FOR_SIMULATOR)
-  if(NOT DEFINED KHEPERAIV_LIBS)
-    message(FATAL_ERROR "Please specify the location of the Khepera IV libraries with -DKHEPERAIV_LIBS=PATH")
-  else(NOT DEFINED KHEPERAIV_LIBS)
-    include_directories(${KHEPERAIV_LIBS}/include)
-    link_directories(${KHEPERAIV_LIBS}/lib)
-  endif(NOT DEFINED KHEPERAIV_LIBS)
+  if(NOT DEFINED TURTLEBOT3_LIBS)
+    message(FATAL_ERROR "Please specify the location of the Turtlebot 3 libraries with -DTURTLEBOT3_LIBS=PATH")
+  else(NOT DEFINED TURTLEBOT3_LIBS)
+    include_directories(${TURTLEBOT3_LIBS}/include)
+    link_directories(${TURTLEBOT3_LIBS}/lib)
+  endif(NOT DEFINED TURTLEBOT3_LIBS)
 endif(NOT ARGOS_BUILD_FOR_SIMULATOR)
